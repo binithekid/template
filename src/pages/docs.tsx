63844a5eedd887c8ccf5dd3ef273a47c5772d3b0
Binyam Teklebrhan <bini.tek1@gmail.com>
2024-06-06 19:56:28 +0100

    first commit

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const docs = () => {
-  return <div>docs</div>;
+  return (
+    <div className="px-[8rem] py-[8rem]">
+      <div className="h-screen"></div>
+    </div>
+  );
 };
 
 export default docs;
